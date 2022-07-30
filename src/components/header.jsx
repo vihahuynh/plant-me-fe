1f9363cee21fb7a59e342856ec1d63c2c789c04c
vihahuynh <huynhviha1703@gmail.com>
2022-07-30 23:34:55 +0700

    header section

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,27 @@
 import styles from "./header.module.scss";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.descriptionContainer}>
+        <h1>Stop there,</h1>
+        <h1>
+          <span>Plant</span> a new tree
+        </h1>
+        <div className={styles.description}>
+          <p>The best time to plant a tree was 20 years ago</p>
+          <p>The second best time is today</p>
+        </div>
+        <a href="#">Discover</a>
+      </div>
+      <img src="/images/header-img.png" alt="large-snake-plant" />
+      <ul className={styles.contact}>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Twitter</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Header;
