879b4af25948904803b543af5a25f57eecfdd9dd
vihahuynh <huynhviha1703@gmail.com>
2022-07-31 19:59:09 +0700

    shop by Caterogies

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,4 +1,33 @@
+import styles from "./categories.module.scss";
+
 const Categories = () => {
-  return <section></section>;
+  return (
+    <section className={styles.container}>
+      <h2>Shop By Categories</h2>
+      <div className={styles.grid}>
+        <div className={styles.firstGrid}>
+          <div>
+            <h5>All</h5>
+            <h5>About Castus</h5>
+          </div>
+          <img src="/images/castus.png" alt="castus" />
+        </div>
+        <div className={styles.secondGrid}>
+          <h5>Outdoor Plants</h5>
+          <img
+            src="/images/philodendronb-birkin-plant.png"
+            alt="philodendronb-birkin-plant"
+          />
+        </div>
+        <div className={styles.thridGrid}>
+          <h5>Indoor Plants</h5>
+          <img
+            src="/images/fittonia-green-plant.png"
+            alt="fittonia green plant"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Categories;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,14 @@ import Header from "../components/header";
 import PopularProducts from "../components/products/popularProducts";
 import Features from "../components/features/features";
 import Blogs from "../components/blogs/blogs";
+import Categories from "../components/categories";
 
 const Home = () => {
   return (
     <>
       <Header />
       <PopularProducts />
+      <Categories />
       <Features />
       <Blogs />
     </>
