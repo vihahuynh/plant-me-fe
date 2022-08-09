5276541da9e930de7a61cf43919fd3f3a953c048
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-08-09 14:20:09 +0700

    span close and open filter animation

--- src/components/UI/drawers/dropdownMenu.jsx
+++ src/components/UI/drawers/dropdownMenu.jsx
@@ -2,8 +2,6 @@ import { useState } from "react"
 
 import styles from './dropdownMenu.module.scss'
 
-import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai/index"
-
 const DropdownMenu = ({ item }) => {
     const [open, setOpen] = useState(false)
 
@@ -11,13 +9,12 @@ const DropdownMenu = ({ item }) => {
     return <div className={styles.container}>
         <div className={styles.header} onClick={() => setOpen(prev => !prev)}>
             <div>{item.text}</div>
-            {open ? <AiOutlineMinus className={styles.icon} /> : <AiOutlinePlus className={styles.icon} />}
+            <span className={open ? styles.minus : ''}></span>
         </div>
         <div className={open ? styles.menuContainer : `${styles.menuContainer} ${styles.hidden}`}>
             {item.subOptions.map(option =>
                 <div key={option.id} className={styles.checkbox}>
-                    <input type="checkbox" id={option.query} name={option.query} value={option.query} />
-
+                    <input className={styles.option} type="checkbox" id={option.query} name={option.query} value={option.query} />
                     <label for={option.query}><span></span>{option.text}</label>
                 </div>
             )}
