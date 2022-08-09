2fcea9bba9c3e3f78ae12ea9841cc50076e6f287
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-08-09 12:48:16 +0700

    update dropdown menu

--- src/components/UI/drawers/dropdownMenu.jsx
+++ src/components/UI/drawers/dropdownMenu.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+
 import styles from './dropdownMenu.module.scss'
 
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai/index"
@@ -6,19 +7,19 @@ import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai/index"
 const DropdownMenu = ({ item }) => {
     const [open, setOpen] = useState(false)
 
+
     return <div className={styles.container}>
         <div className={styles.header} onClick={() => setOpen(prev => !prev)}>
             <div>{item.text}</div>
             {open ? <AiOutlineMinus className={styles.icon} /> : <AiOutlinePlus className={styles.icon} />}
         </div>
-        <div className={styles.menuContainer}>
-            <div className={open ? styles.menu : `${styles.menu} ${styles.hidden}`}>
-                {item.subOptions.map(option =>
-                    <div key={option.id} className={styles.checkbox}>
-                        <input type="checkbox" id={option.query} name={option.query} value={option.query} />
-                        <label for={option.query}>{option.text}</label>
-                    </div>)}
-            </div>
+        <div className={open ? styles.menuContainer : `${styles.menuContainer} ${styles.hidden}`}>
+            {item.subOptions.map(option =>
+                <div key={option.id} className={styles.checkbox}>
+                    <input type="checkbox" id={option.query} name={option.query} value={option.query} />
+                    <label for={option.query}>{option.text}</label>
+                </div>
+            )}
         </div>
     </div >
 }
--- src/components/UI/drawers/filterProducts.jsx
+++ src/components/UI/drawers/filterProducts.jsx
@@ -1,15 +1,14 @@
-import Drawer from '@mui/material/Drawer';
 import { useState } from 'react';
 
-import { filterOptions } from '../../../data';
-
-import styles from "./filterProducts.module.scss"
+import Drawer from '@mui/material/Drawer';
 import { VscChromeClose } from "react-icons/vsc/index"
+
+import { filterOptions } from '../../../data';
+import styles from "./filterProducts.module.scss"
 import DropdownMenu from './dropdownMenu';
 
 const FilterProducts = () => {
     const [open, setOpen] = useState(false)
-    const [sort, setSort] = useState(null)
 
     return <div>
         <button onClick={() => setOpen(true)}>Filters</button>
