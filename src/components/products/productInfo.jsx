faaf41a4c5b87c5a92829d12dfdba847c091d958
vihahuynh <huynhviha1703@gmail.com>
2022-08-11 21:04:52 +0700

    add quantity

--- src/components/products/productInfo.jsx
+++ src/components/products/productInfo.jsx
@@ -5,9 +5,18 @@ import Price from "./price";
 import styles from "./productInfo.module.scss";
 import Button from "./../UI/button";
 
+import {
+  AiOutlineMinusCircle,
+  AiOutlinePlusCircle,
+} from "react-icons/ai/index";
+
 const ProductInfo = ({ product }) => {
   const [size, setSize] = useState(null);
   const [color, setColor] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+
+  const onMinus = () => setQuantity((prev) => (prev - 1 > 0 ? prev - 1 : 1));
+  const onPlus = () => setQuantity((prev) => prev + 1);
 
   return (
     <>
@@ -68,7 +77,20 @@ const ProductInfo = ({ product }) => {
           </ul>
         </div>
       )}
-      <div></div>
+      <div className={styles.quantityContainer}>
+        <span onClick={onMinus}>
+          <AiOutlineMinusCircle className={styles.icon} />
+        </span>
+        <input
+          className={styles.quantityInput}
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(+e.value.target)}
+        />
+        <span onClick={onPlus}>
+          <AiOutlinePlusCircle className={styles.icon} />
+        </span>
+      </div>
       <Button text="Add to cart" size="medium" className={styles.btn} />
     </>
   );
