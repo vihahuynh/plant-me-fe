e0f44e612bdd36e186de65bbf4107c3e54bd02b5
vihahuynh <huynhviha1703@gmail.com>
2022-08-12 21:35:31 +0700

    update styles

--- src/components/products/buyInfo.jsx
+++ src/components/products/buyInfo.jsx
@@ -19,79 +19,89 @@ const BuyInfo = ({ product }) => {
   const onPlus = () => setQuantity((prev) => prev + 1);
 
   return (
-    <div>
-      <h3 className={styles.title}>{product.title}</h3>
-      <div className={styles.statisticContainer}>
-        <span className={styles.statistic}>
-          <Rating name="read-only" value={product.rating} readOnly />
-        </span>
-        <span className={styles.statistic}>{product.reviewCount} reviews</span>
-        <span className={styles.statistic}>Sold: {product.soldCount}</span>
-      </div>
-      <Price
-        price={product.price}
-        salePercent={product.salePercent}
-        size="big"
-      />
-      {!!product.size && (
-        <div className={styles.optionContainer}>
-          <p className={styles.optionTitle}>Size</p>
-          <ul className={styles.optionList}>
-            {product?.size?.map((s) => (
-              <li
-                className={
-                  size !== s
-                    ? styles.sizeItem
-                    : `${styles.sizeItem} ${styles.active}`
-                }
-                key={s}
-                onClick={() => setSize(s)}
-              >
-                {s}
-              </li>
-            ))}
-          </ul>
+    <>
+      <div>
+        <h3 className={styles.title}>{product.title}</h3>
+        <div className={styles.statisticContainer}>
+          <span className={styles.statistic}>
+            <Rating name="read-only" value={product.rating} readOnly />
+          </span>
+          <span className={styles.statistic}>
+            {product.reviewCount} reviews
+          </span>
+          <span className={styles.statistic}>Sold: {product.soldCount}</span>
         </div>
-      )}
-      {!!product.colors && (
-        <div className={styles.optionContainer}>
-          <p className={styles.optionTitle}>Color</p>
-          <ul className={styles.optionList}>
-            {product?.colors?.map((c) => (
-              <li
-                style={{
-                  color: c,
-                  outline: `1px solid ${c}`,
-                  backgroundColor: c,
-                }}
-                className={
-                  color !== c
-                    ? styles.colorItem
-                    : `${styles.colorItem} ${styles.active}`
-                }
-                key={c}
-                onClick={() => setColor(c)}
-              ></li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <div className={styles.quantityContainer}>
-        <span onClick={onMinus}>
-          <AiOutlineMinusCircle className={styles.icon} />
-        </span>
-        <input
-          className={styles.quantityInput}
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(+e.value.target)}
+        <Price
+          price={product.price}
+          salePercent={product.salePercent}
+          size="big"
         />
-        <span onClick={onPlus}>
-          <AiOutlinePlusCircle className={styles.icon} />
-        </span>
       </div>
-      <Button text="Add to cart" size="medium" className={styles.btn} />
-    </div>
+
+      <div>
+        {!!product.size && (
+          <div className={styles.optionContainer}>
+            <p className={styles.optionTitle}>Size</p>
+            <ul className={styles.optionList}>
+              {product?.size?.map((s) => (
+                <li
+                  className={
+                    size !== s
+                      ? styles.sizeItem
+                      : `${styles.sizeItem} ${styles.active}`
+                  }
+                  key={s}
+                  onClick={() => setSize(s)}
+                >
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {!!product.colors && (
+          <div className={styles.optionContainer}>
+            <p className={styles.optionTitle}>Color</p>
+            <ul className={styles.optionList}>
+              {product?.colors?.map((c) => (
+                <li
+                  style={{
+                    color: c,
+                    outline: `1px solid ${c}`,
+                    backgroundColor: c,
+                  }}
+                  className={
+                    color !== c
+                      ? styles.colorItem
+                      : `${styles.colorItem} ${styles.active}`
+                  }
+                  key={c}
+                  onClick={() => setColor(c)}
+                ></li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <div className={styles.quantityContainer}>
+          <span onClick={onMinus}>
+            <AiOutlineMinusCircle className={styles.icon} />
+          </span>
+          <input
+            className={styles.quantityInput}
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(+e.value.target)}
+          />
+          <span onClick={onPlus}>
+            <AiOutlinePlusCircle className={styles.icon} />
+          </span>
+        </div>
+        <Button text="Add to cart" size="medium" className={styles.btn} />
+      </div>
+    </>
   );
 };
 
