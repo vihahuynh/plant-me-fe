15d1d67aa4c9eb5a2c9ad420189cbf4927e20606
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-08-12 16:18:00 +0700

    review summary and progress bar

---
+++ src/components/UI/progressBar.jsx
@@ -0,0 +1,7 @@
+import styles from "./progressBar.module.scss"
+
+const ProgressBar = ({ percent }) => {
+    return <span className={styles.container}><span className={styles.percent} style={{ width: `${percent}%` }}></span></span>
+}
+
+export default ProgressBar
--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -1,8 +1,46 @@
+import { Rating } from "@mui/material";
 import { reviews } from "../../data";
+import ProgressBar from "../UI/progressBar";
 import ReviewItem from "./reviewItem";
 
+import styles from "./reviews.module.scss"
+
 const Reviews = () => {
-    return <div>
+    const ratingStatistics = {
+        total: reviews.length,
+        average: reviews.reduce((average, review) => {
+            return average + review.rating / reviews.length
+        }, 0),
+        count: [
+            reviews.filter(r => r.rating === 5).length,
+            reviews.filter(r => r.rating === 4).length,
+            reviews.filter(r => r.rating === 3).length,
+            reviews.filter(r => r.rating === 2).length,
+            reviews.filter(r => r.rating === 1).length,
+        ]
+    }
+
+    console.log(ratingStatistics)
+    return <div className={styles.container}>
+        <h2>Reviews</h2>
+        <div className={styles.summary}>
+            <div className={styles.ratingStatistics}>
+                <span className={styles.average}>{ratingStatistics.average}</span>
+                <Rating className={styles.averageStars} name="read-only" value={ratingStatistics.average} readOnly />
+                <p className={styles.total}>{ratingStatistics.total} reviews</p>
+                <ul className={styles.count}>{ratingStatistics.count.map((r, id) =>
+                    <li key={id}>
+                        <Rating name="read-only" value={5 - id} readOnly />
+                        <ProgressBar percent={r / ratingStatistics.total * 100} />
+                        <span>{r}</span>
+                    </li>
+                )}
+                </ul>
+            </div>
+            <div className={styles.filters}>
+
+            </div>
+        </div>
         {reviews.map(review => <ReviewItem key={review.id} review={review} />)}
     </div>
 }
