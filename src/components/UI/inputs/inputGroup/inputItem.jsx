cdcc2dd26b45bb61001c9ee2b86b6018e6f65495
vihahuynh <huynhviha1703@gmail.com>
2022-09-10 17:28:41 +0700

    new product form: validate sub-form

--- src/components/UI/inputs/inputGroup/inputItem.jsx
+++ src/components/UI/inputs/inputGroup/inputItem.jsx
@@ -1,49 +1,95 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import styles from "./inputItem.module.scss";
 import { RiSave3Line, RiCloseLine } from "react-icons/ri/index";
 
-const InputItem = ({ item, inputTitle, onSave, onCancel }) => {
-  const [title, setTitle] = useState(item.title);
-  const [content, setContent] = useState(item.content);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        value: action.value,
+        isValid: !!action.value.length,
+        isTouched: true,
+      };
+    case "TOUCHED":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
 
-  const handleChangeTitle = (e) => setTitle(e.target.value);
-  const handleChangeContent = (e) => setContent(e.target.value);
+const InputItem = ({ item, inputTitle, onSave, onCancel }) => {
+  const [titleState, dispatchTitle] = useReducer(reducer, {
+    value: item.title,
+    isValid: false,
+    isTouched: false,
+  });
+  const [contentState, dispatchContent] = useReducer(reducer, {
+    value: item.content,
+    isValid: false,
+    isTouched: false,
+  });
+
+  const handleChangeTitle = (e) =>
+    dispatchTitle({ type: "CHANGE", value: e.target.value });
+
+  const handleChangeContent = (e) =>
+    dispatchContent({ type: "CHANGE", value: e.target.value });
 
   const onCancelEdit = () => {
     onCancel();
-    setTitle(item.title);
-    setContent(item.content);
+    dispatchTitle({ type: "CHANGE", value: item.title });
+    dispatchContent({ type: "CHANGE", value: item.content });
+  };
+
+  const onSaveItem = () => {
+    if (titleState.isValid && contentState.isValid) {
+      onSave(titleState.value, contentState.value);
+    } else {
+      dispatchContent({ type: "TOUCHED" });
+      dispatchTitle({ type: "TOUCHED" });
+    }
   };
 
   return (
     <div className={styles.formItem}>
+      <h5>
+        {inputTitle}
+        <div>
+          <RiSave3Line
+            className={`${styles.icon} ${styles.iconBlue}`}
+            onClick={onSaveItem}
+          />
+          <RiCloseLine
+            className={`${styles.icon} ${styles.iconRed}`}
+            onClick={onCancelEdit}
+          />
+        </div>
+      </h5>
       <div className={styles.inputContainer}>
-        <h5>
-          {inputTitle}
-          <div>
-            <RiSave3Line
-              className={`${styles.icon} ${styles.iconBlue}`}
-              onClick={() => onSave(title, content)}
-            />
-            <RiCloseLine
-              className={`${styles.icon} ${styles.iconRed}`}
-              onClick={onCancelEdit}
-            />
-          </div>
-        </h5>
         <input
           type="text"
           placeholder="Title"
-          value={title}
+          value={titleState.value}
           onChange={handleChangeTitle}
         />
+        {!!titleState.isTouched && !titleState.isValid && (
+          <p className={styles.errors}>Title is required</p>
+        )}
+      </div>
+      <div className={styles.inputContainer}>
         <textarea
           rows={3}
           name="Content"
           placeholder="Content"
-          value={content}
+          value={contentState.value}
           onChange={handleChangeContent}
         />
+        {!!contentState.isTouched && !contentState.isValid && (
+          <p className={styles.errors}>Content is required</p>
+        )}
       </div>
     </div>
   );
