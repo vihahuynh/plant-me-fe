790c1301048ec74e7db933b972513ecafbaedcb1
vihahuynh <huynhviha1703@gmail.com>
2022-09-24 12:58:37 +0700

    update components: userReviews and userReviewItem

--- src/components/reviews/userReviewItem.jsx
+++ src/components/reviews/userReviewItem.jsx
@@ -1,5 +1,45 @@
+import { Rating } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { AiOutlineLike } from "react-icons/ai";
+
+import styles from "./userReviewItem.module.scss";
+
 const UserReviewItem = ({ item }) => {
-  return <div></div>;
+  return (
+    <div className={styles.userReviewItem}>
+      <img
+        src={
+          item.product.images.find((img) => img.includes("eye")) ||
+          item.product.images[0]
+        }
+        alt=""
+      />
+
+      <div className={styles.reviewContent}>
+        <Link to={`/products/${item.product.id}`}>
+          <h5 className={styles.productTitle}>{item.product.title}</h5>
+        </Link>
+        <Rating
+          className={styles.rating}
+          name="read-only"
+          value={item.rating}
+          readOnly
+        />
+        <h5>{item.title}</h5>
+        <p>{item.content}</p>
+        <div className={styles.iconBox}>
+          <AiOutlineLike className={styles.icon} />
+          <span>{item.like}</span>
+        </div>
+      </div>
+
+      <div>
+        {!!item.images.length &&
+          item.images.map((img) => <img src={img} alt="" />)}
+      </div>
+    </div>
+  );
 };
 
 export default UserReviewItem;
--- src/components/reviews/userReviews.jsx
+++ src/components/reviews/userReviews.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import reviewService from "./../../services/review";
+import UserReviewItem from "./userReviewItem";
+
+import styles from "./userReviews.module.scss";
+
 const UserReviews = () => {
-  return <div></div>;
+  const [reviews, setReviews] = useState([]);
+  const authen = useSelector((state) => state.authentication);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const reviewsData = await reviewService.getAll({
+        userId: authen?.user?.id,
+      });
+      setReviews(reviewsData.data);
+    };
+    fetchData();
+  }, [authen?.user?.id]);
+  console.log(reviews);
+
+  return (
+    <ul className={styles.userReviewsList}>
+      {reviews.map((item) => (
+        <UserReviewItem item={item} />
+      ))}
+    </ul>
+  );
 };
 
 export default UserReviews;
