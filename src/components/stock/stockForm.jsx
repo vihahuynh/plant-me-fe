0ff7a628cd4e49d5c9d38c8348540784eb99092f
vihahuynh <huynhviha1703@gmail.com>
2022-10-12 21:22:17 +0700

    update stocks

--- src/components/stock/stockForm.jsx
+++ src/components/stock/stockForm.jsx
@@ -1,143 +1,172 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import styles from "./stockForm.module.scss"
+import styles from "./stockForm.module.scss";
 
 import { SketchPicker } from "react-color";
 import { Formik } from "formik";
-import Button from "./../UI/buttons/button"
+import Button from "./../UI/buttons/button";
 import stockSerice from "../../services/stock";
 import { useSelector } from "react-redux";
 
 const StockForm = ({ stock, productId, onCancel, setStocks }) => {
-    const authen = useSelector(state => state.authentication)
-    const [currentColor, setCurrentColor] = useState('#fff')
-    const [currentSize, setCurrentSize] = useState("M")
+  const authen = useSelector((state) => state.authentication);
+  const [currentColor, setCurrentColor] = useState("#fff");
+  const [currentSize, setCurrentSize] = useState("M");
 
-    useEffect(() => {
-        setCurrentColor(stock?.color || '#fff')
-        setCurrentSize(stock?.size || "M")
-    }, [stock])
+  useEffect(() => {
+    setCurrentColor(stock?.color || "#fff");
+    setCurrentSize(stock?.size || "M");
+  }, [stock]);
 
-    const onUpdateStock = async (values) => {
-        try {
-            const stockToUpdate = {
-                ...stock,
-                color: currentColor,
-                size: currentSize,
-                quantity: values.quantity
-            }
-            const updatedStock = await stockSerice.update(stock?.id, stockToUpdate, authen)
-            setStocks(prev => prev.map(item => item.id !== stock.id ? item : updatedStock.data))
-
-        } catch (err) {
-            console.log(err)
-        } finally {
-            onCancel()
-        }
-
+  const onUpdateStock = async (values) => {
+    try {
+      const stockToUpdate = {
+        ...stock,
+        color: currentColor,
+        size: currentSize,
+        quantity: values.quantity,
+      };
+      const updatedStock = await stockSerice.update(
+        stock?.id,
+        stockToUpdate,
+        authen
+      );
+      setStocks((prev) =>
+        prev.map((item) => (item.id !== stock.id ? item : updatedStock.data))
+      );
+    } catch (err) {
+      console.log(err);
+    } finally {
+      onCancel();
     }
+  };
 
-    const onAddNewStock = async (values) => {
-        try {
-            const newStock = {
-                product: productId,
-                color: currentColor,
-                size: currentSize,
-                quantity: values.quantity
-            }
-            const returnedStock = await stockSerice.create(newStock, authen?.user?.token)
-            setStocks(prev => prev.concat(returnedStock.data))
-        } catch (err) {
-            console.log(err)
-        }
-        finally {
-            onCancel()
-        }
+  const onAddNewStock = async (values) => {
+    try {
+      const newStock = {
+        product: productId,
+        color: currentColor,
+        size: currentSize,
+        quantity: values.quantity,
+      };
+      const returnedStock = await stockSerice.create(
+        newStock,
+        authen?.user?.token
+      );
+      setStocks((prev) => prev.concat(returnedStock.data));
+    } catch (err) {
+      console.log(err);
+    } finally {
+      onCancel();
     }
+  };
 
-    return <div>
-        <div className={styles.formContainer}>
-            <Formik
-                initialValues={stock || {
-                    color: currentColor,
-                    size: currentSize,
-                    quantity: stock?.quantity || 1
-                }}
-                validate={(values) => {
-                    const errors = {};
-                    if (!values.color.length) {
-                        errors.color = "Colors is required";
-                    }
-                    if (!values.size) {
-                        errors.size = "Size is required";
-                    }
-                    if (!values.quantity) {
-                        errors.quantity = "Quantity is required";
-                    } else if (values.quantity < 0) {
-                        errors.quantity = "Invalid quantity"
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        if (stock?.id ? onUpdateStock(values) : onAddNewStock(values))
-                            setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-                }) => (
-                    <form onSubmit={handleSubmit} className={styles.form}>
-                        <div className={`${styles.inputContainer} ${styles.size}`}>
-                            <label>Size</label>
-                            <ul className={styles.sizeList}>
-                                {["XS", "S", "M", "L", "XL"].map(item =>
-                                    <li className={`${styles.sizeItem} ${currentSize === item ? styles.active : ""}`} key={item} onClick={() => setCurrentSize(item)}>{item}</li>
-                                )}
-                            </ul>
-                            <p className={styles.errors}>
-                                {errors.size && touched.size && errors.size}
-                            </p>
-                        </div>
-                        <div className={`${styles.inputContainer} ${styles.quantity}`}>
-                            <label>Quantity</label>
-                            <input
-                                id="quantity"
-                                type="text"
-                                name="quantity"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.quantity}
-                                placeholder="Quantity"
-                            />
-                            <p className={styles.errors}>
-                                {errors.quantity && touched.quantity && errors.quantity}
-                            </p>
-                        </div>
-                        <div className={`${styles.inputContainer} ${styles.colors}`}>
-                            <label>Color</label>
-                            <SketchPicker
-                                disableAlpha={true}
-                                color={currentColor}
-                                onChangeComplete={(color, _) => setCurrentColor(color.hex)}
-                            />
-                        </div>
-                        <div className={styles.btnGroup}>
-                            <Button text="Save" size="small" borderRadius="square" disabled={isSubmitting} />
-                            <Button text="Cancel" size="small" borderRadius="square" theme="light" onClick={onCancel} />
-                        </div>
-                    </form>
-                )}
-            </Formik>
-        </div>
-    </div >
-}
+  return (
+    <div>
+      <div className={styles.formContainer}>
+        <Formik
+          initialValues={
+            stock || {
+              color: currentColor,
+              size: currentSize,
+              quantity: stock?.quantity || 1,
+            }
+          }
+          validate={(values) => {
+            const errors = {};
+            if (!values.color.length) {
+              errors.color = "Colors is required";
+            }
+            if (!values.size) {
+              errors.size = "Size is required";
+            }
+            if (!values.quantity) {
+              errors.quantity = "Quantity is required";
+            } else if (values.quantity < 0) {
+              errors.quantity = "Invalid quantity";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              if (stock?.id ? onUpdateStock(values) : onAddNewStock(values))
+                setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit} className={styles.form}>
+              <div className={`${styles.inputContainer} ${styles.size}`}>
+                <label>Size</label>
+                <ul className={styles.sizeList}>
+                  {["XS", "S", "M", "L", "XL"].map((item) => (
+                    <li
+                      className={`${styles.sizeItem} ${
+                        currentSize === item ? styles.active : ""
+                      }`}
+                      key={item}
+                      onClick={() => setCurrentSize(item)}
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+                <p className={styles.errors}>
+                  {errors.size && touched.size && errors.size}
+                </p>
+              </div>
+              <div className={`${styles.inputContainer} ${styles.quantity}`}>
+                <label>Quantity</label>
+                <input
+                  id="quantity"
+                  type="text"
+                  name="quantity"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.quantity}
+                  placeholder="Quantity"
+                />
+                <p className={styles.errors}>
+                  {errors.quantity && touched.quantity && errors.quantity}
+                </p>
+              </div>
+              <div className={`${styles.inputContainer} ${styles.colors}`}>
+                <label>Color</label>
+                <SketchPicker
+                  disableAlpha={true}
+                  color={currentColor}
+                  onChangeComplete={(color, _) => setCurrentColor(color.hex)}
+                />
+              </div>
+              <div className={styles.btnGroup}>
+                <Button
+                  text="Save"
+                  size="small"
+                  borderRadius="square"
+                  disabled={isSubmitting}
+                />
+                <Button
+                  text="Cancel"
+                  size="small"
+                  borderRadius="square"
+                  theme="light"
+                  onClick={onCancel}
+                />
+              </div>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
 
-export default StockForm
+export default StockForm;
--- src/data.js
+++ src/data.js
@@ -1,38 +1,3 @@
-export const products = [
-  {
-    id: 1,
-    name: "Jade Plant Mini",
-    imageUrl: "/images/jade-plant-mini.png",
-    price: 399,
-    salePercent: 25,
-    like: true,
-  },
-  {
-    id: 2,
-    name: "Broken Heart Plant",
-    imageUrl: "/images/broken-heart-plant.png",
-    price: 499,
-    salePercent: 20,
-    like: false,
-  },
-  {
-    id: 3,
-    name: "Peperomia Variegated Plant",
-    imageUrl: "/images/peperomia-variegated-plant.png",
-    price: 399,
-    salePercent: 25,
-    like: false,
-  },
-  {
-    id: 4,
-    name: "Aralia Variegated Mini Plant",
-    imageUrl: "/images/aralia-variegated-mini-plant.png",
-    price: 399,
-    salePercent: 13,
-    like: true,
-  },
-];
-
 export const mainFeatures = [
   {
     id: 1,
@@ -253,17 +218,17 @@ export const plantsFilterOptions = [
       {
         id: 1,
         text: "Low Light",
-        query: "light=low light",
+        query: "livingConditions.content=low light",
       },
       {
         id: 2,
         text: "Bright Indirect Light",
-        query: "light=bright indirect light",
+        query: "livingConditions.content=bright indirect light",
       },
       {
         id: 3,
         text: "Direct Light",
-        query: "light=direct light",
+        query: "livingConditions.content=direct light",
       },
     ],
   },
@@ -404,7 +369,6 @@ export const reviewsFilterOptions = [
   },
 ];
 
-
 export const notificationSortOptions = [
   {
     id: 1,
@@ -425,7 +389,7 @@ export const notificationSortOptions = [
     id: 4,
     query: "sortBy=isRead:desc",
     text: "Show unread notification last",
-  }
+  },
 ];
 
 export const notificationFilterOptions = [
@@ -449,7 +413,7 @@ export const notificationFilterOptions = [
         id: 3,
         text: "General",
         query: "type=general",
-      }
+      },
     ],
   },
   {
