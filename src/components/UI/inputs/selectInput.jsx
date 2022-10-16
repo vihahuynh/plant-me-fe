ec93c44bf80ca750209274039ad505df5c257701
vihahuynh <huynhviha1703@gmail.com>
2022-10-16 15:59:34 +0700

    update component: select input

--- src/components/UI/inputs/selectInput.jsx
+++ src/components/UI/inputs/selectInput.jsx
@@ -7,7 +7,7 @@ const SelectInput = ({
   setCurrentOption,
   multiple = false,
 }) => {
-  const [openDataList, setOpenDataList] = useState(false)
+  const [openDataList, setOpenDataList] = useState(false);
   const onSelectMultipleData = (value) => {
     if (currentOption.includes(value)) {
       setCurrentOption((prev) => prev.filter((op) => op !== value));
@@ -16,37 +16,30 @@ const SelectInput = ({
     }
   };
 
-  const toggleOpenDataList = () => setOpenDataList(prev => !prev)
+  const toggleOpenDataList = () => setOpenDataList((prev) => !prev);
 
   if (!multiple) {
     return (
       <div className={styles.container}>
         <div className={styles.optionBox} onClick={toggleOpenDataList}>
-          <ul className={styles.choosenDataList} data-num-item={listData.length}>
-            {currentOption ? <li
-              className={styles.choosenDataItem}
-              key={currentOption}
-              onClick={() => setCurrentOption(currentOption)}
-            >
-              {currentOption}
-            </li>
-              : <p className={styles.text}>Please select an option</p>
-            }
-          </ul>
+          <p className={styles.chosenText}>
+            {currentOption || "Please select an option"}
+          </p>
         </div>
-        <ul className={styles.dataList} style={{ maxHeight: openDataList ? `${4 * listData.length}rem` : 0 }} >
-          {
-            listData.map((item) => (
-              <li
-                className={`${styles.dataItem} ${currentOption === item ? styles.active : ""
-                  }`}
-                key={item}
-                onClick={() => setCurrentOption(item)}
-              >
-                {item}
-              </li>
-            ))
-          }
+        <ul
+          className={`${styles.dataList} ${openDataList ? styles.active : ""}`}
+        >
+          {listData.map((item) => (
+            <li
+              className={`${styles.dataItem} ${
+                currentOption === item ? styles.active : ""
+              }`}
+              key={item}
+              onClick={() => setCurrentOption(item)}
+            >
+              {item}
+            </li>
+          ))}
         </ul>
       </div>
     );
@@ -54,25 +47,16 @@ const SelectInput = ({
   return (
     <div className={styles.container}>
       <div className={styles.optionBox} onClick={toggleOpenDataList}>
-        <ul className={styles.choosenDataList} data-num-item={listData.length}>
-          {currentOption.length ?
-            currentOption.map(item => <li
-              className={styles.choosenDataItem}
-              key={item}
-              onClick={() => setCurrentOption(item)}
-            >
-              {item}
-            </li>
-            )
-            : <p className={styles.text}>Please select an option</p>
-          }
-        </ul>
+        <p className={styles.chosenText}>
+          {currentOption?.join(", ") || "Please select an option"}
+        </p>
       </div>
-      <ul className={styles.dataList} style={{ maxHeight: openDataList ? `${4 * listData.length}rem` : 0 }} >
+      <ul className={`${styles.dataList} ${openDataList ? styles.active : ""}`}>
         {listData.map((item) => (
           <li
-            className={`${styles.dataItem} ${currentOption.includes(item) ? styles.active : ""
-              }`}
+            className={`${styles.dataItem} ${
+              currentOption.includes(item) ? styles.active : ""
+            }`}
             key={item}
             onClick={() => onSelectMultipleData(item)}
           >
@@ -81,7 +65,7 @@ const SelectInput = ({
         ))}
       </ul>
     </div>
-  )
+  );
 };
 
 export default SelectInput;
