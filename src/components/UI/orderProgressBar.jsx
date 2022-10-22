fd7f75c6a06faf89516ea498787b0a5172090334
vihahuynh <huynhviha1703@gmail.com>
2022-10-22 09:35:34 +0700

    update orderProgressbar

--- src/components/UI/orderProgressBar.jsx
+++ src/components/UI/orderProgressBar.jsx
@@ -1,35 +1,19 @@
-import styles from "./orderProgressBar.module.scss"
+import Moment from "react-moment";
+import styles from "./orderProgressBar.module.scss";
 
-const steps = [
-  {
-    id: 1,
-    label: 'Select campaign settings',
-    time: '2021-02-01'
-  },
-  {
-    id: 2,
-    label: 'Create an ad group',
-    time: '2021-02-01'
-  },
-  {
-    id: 3,
-    label: 'Create an ad',
-    time: '2021-02-01'
-  },
-];
-
-const OrderProgressBar = () => {
+const OrderProgressBar = ({ steps }) => {
+  if (!steps?.length) return null;
   return (
     <ul className={styles.stepList}>
-      {steps.map(step =>
+      {steps.map((step) => (
         <li key={step.id} className={styles.stepItem}>
-          <p className={styles.stepTitle}>{step.label}</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing eles, sapiente veniam quasi quod deleniti delectus neque.</p>
-          <p>{step.time}</p>
+          <p className={styles.stepTitle}>{step.title}</p>
+          <p>{step.description}</p>
+          <Moment format="YYYY-MM-DD hh:mm:ss">{step.createdAt}</Moment>
         </li>
-      )}
+      ))}
     </ul>
   );
-}
+};
 
-export default OrderProgressBar
+export default OrderProgressBar;
--- src/pages/user/orderTracking.jsx
+++ src/pages/user/orderTracking.jsx
@@ -1,10 +1,40 @@
 import Wrapper from "../../components/layout/wrapper";
 import OrderProgressBar from "./../../components/UI/orderProgressBar";
+import UserLeftMenu from "./../../components/layout/userLetfMenu/userLeftMenu";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import orderService from "./../../services/order";
+
+import styles from "./orderTracking.module.scss";
 
 const OrderTracking = () => {
+  const { orderId } = useParams();
+  const authen = useSelector((state) => state.authentication);
+  const [order, setOrder] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const orderData = await orderService.get(orderId, authen?.user?.token);
+      setOrder(orderData.data);
+    };
+    fetchData();
+  }, [orderId, authen?.user?.token]);
+
+  console.log("order: ", order);
+  if (!order) return <p>No order found</p>;
+  if (order?.user !== authen?.user?.id) return <p>Permission denied</p>;
+
   return (
     <Wrapper>
-      <OrderProgressBar />
+      <div className={styles.main}>
+        <UserLeftMenu />
+        <div className={styles.container}>
+          <h3>{order?.status}</h3>
+          <OrderProgressBar steps={order?.progress} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
