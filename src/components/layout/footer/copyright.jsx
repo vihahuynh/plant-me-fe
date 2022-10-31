dbe1ebf8b510ae61664b041f6acbdaf21718c8e8
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-10-31 16:04:38 +0700

    copyright

--- src/components/layout/footer/copyright.jsx
+++ src/components/layout/footer/copyright.jsx
@@ -3,13 +3,17 @@ import styles from "./copyright.module.scss";
 const Copyright = () => {
   return (
     <div className={styles.container}>
-      <p>&copy; plantme.com - All rights reserved.</p>
-      <p>Design by Ananthu Dileep</p>
-      <ul className={styles.list}>
-        <li>Privacy</li>
-        <li>Security</li>
-        <li>Terms</li>
-      </ul>
+      <div className={styles.topContainer}>
+        <p>&copy; plantme.com - All rights reserved.</p>
+        <p>Design by Ananthu Dileep and Huynh Vi Ha</p>
+        <ul className={styles.list}>
+          <li>Privacy</li>
+          <li>Security</li>
+          <li>Terms</li>
+        </ul>
+
+      </div>
+      <p>Image and product information taken from website <a href="https://www.ugaoo.com">https://www.ugaoo.com/</a> (for learning purposes only)</p>
     </div>
   );
 };
