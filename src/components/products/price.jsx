f6e94aaf9578d23d0113db94e84e14893e94dd8f
vihahuynh <huynhviha1703@gmail.com>
2022-11-02 00:35:56 +0700

    update price

--- src/components/products/price.jsx
+++ src/components/products/price.jsx
@@ -2,8 +2,10 @@ import styles from "./price.module.scss";
 
 const Price = ({ price, salePercent = 0, size = "medium", range = false }) => {
   const netPrice = Math.round(price - (price * salePercent) / 100);
-  let minNetPrice
-  let maxNetPrice
+  let minNetPrice;
+  let maxNetPrice;
+  let minGrossPrice;
+  let maxGrossPrice;
 
   let className;
   if (size === "medium") className = styles.priceMediumContainer;
@@ -11,38 +13,52 @@ const Price = ({ price, salePercent = 0, size = "medium", range = false }) => {
   else className = styles.priceLargeContainer;
 
   if (range) {
-    minNetPrice = Math.round(netPrice / 100 * 70)
-    maxNetPrice = Math.round(netPrice / 100 * 130)
+    minNetPrice = Math.round((netPrice / 100) * 70);
+    maxNetPrice = Math.round((netPrice / 100) * 130);
+    minGrossPrice = Math.round((price / 100) * 70);
+    maxGrossPrice = Math.round((price / 100) * 130);
   }
 
-  return (<>
-    {!range
-      ? <div className={className}>
-        {!!salePercent ? (
-          <>
-            <span className={styles.price}>{price}.000 &#x20ab;</span>
-            <span className={styles.priceNet}>{netPrice}.000 &#x20ab;</span>
-            <span className={styles.salePercent}>(&minus;{salePercent}%)</span>
-          </>
-        ) : (
-          <span className={styles.priceNet}>{price}.000 &#x20ab;</span>
-        )}
-      </div>
-      : <div className={className}>
-        {!!salePercent ? (
-          <div className={styles.range}>
-            <span className={styles.priceNet}>{minNetPrice}.000 &#x20ab; - {maxNetPrice}.000 &#x20ab;</span>
-            <div className={styles.subInfo}>
+  return (
+    <>
+      {!range ? (
+        <div className={className}>
+          {!!salePercent ? (
+            <>
               <span className={styles.price}>{price}.000 &#x20ab;</span>
-              <span className={styles.salePercent}>(&minus;{salePercent}%)</span>
+              <span className={styles.priceNet}>{netPrice}.000 &#x20ab;</span>
+              <span className={styles.salePercent}>
+                (&minus;{salePercent}%)
+              </span>
+            </>
+          ) : (
+            <span className={styles.priceNet}>{price}.000 &#x20ab;</span>
+          )}
+        </div>
+      ) : (
+        <div className={className}>
+          {!!salePercent ? (
+            <div className={styles.range}>
+              <span className={styles.priceNet}>
+                {minNetPrice}.000 &#x20ab; - {maxNetPrice}.000 &#x20ab;
+              </span>
+              <div className={styles.subInfo}>
+                <span className={styles.price}>
+                  {minGrossPrice}.000 &#x20ab; - {maxGrossPrice}.000 &#x20ab;
+                </span>
+                <span className={styles.salePercent}>
+                  (&minus;{salePercent}%)
+                </span>
+              </div>
             </div>
-          </div>
-        ) : (
-          <span className={styles.priceNet}>{minNetPrice}.000 &#x20ab; - {maxNetPrice}.000 &#x20ab;</span>
-        )}
-      </div>
-    }
-  </>
+          ) : (
+            <span className={styles.priceNet}>
+              {minNetPrice}.000 &#x20ab; - {maxNetPrice}.000 &#x20ab;
+            </span>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
